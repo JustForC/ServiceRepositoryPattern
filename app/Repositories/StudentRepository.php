43002fb59cb840e0b36f5f831bca4c2acf507de1
justforc <ghemaallan@gmail.com>
2022-04-05 12:34:01 +0700

    Delete Service

--- app/Repositories/StudentRepository.php
+++ app/Repositories/StudentRepository.php
@@ -2,7 +2,21 @@
 
 namespace App\Repositories;
 
+use App\Models\Student;
+
 class StudenRepository
 {
+    protected $student;
+
+    public function __construct(Student $student)
+    {
+        $this->student = $student;
+    }
+
+    public function deleteId($id)
+    {
+        $student = $this->student->find($id)->delete();
+        return $student;
+    }
 
 }
--- app/Services/StudentService.php
+++ app/Services/StudentService.php
@@ -2,7 +2,36 @@
 
 namespace App\Services;
 
+use App\Repositories\StudenRepository;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
 class StudentService
 {
+
+    protected $repository;
+
+    public function __construct(StudenRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function deleteById($id)
+    {
+        DB::beginTransaction();
+
+        try
+        {
+            $student = $this->repository->deleteId($id);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::info($e->getMessage());
+        }
+
+        DB::commit();
+
+        return $student;
+    }
 
 }
